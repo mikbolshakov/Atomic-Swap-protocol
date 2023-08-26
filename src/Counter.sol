01fb233bbb55264423ad3c9c78f80443528fcbae
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2023-08-26 07:36:40 +0300

    dev

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
+//     solc = '0.8.19'
+// optimizer = true
+// optimizer_runs = 200
+// remappings = ["@openzeppelin/=node_modules/@openzeppelin/contracts"]
+
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
